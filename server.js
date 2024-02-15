8f09e9a243aad64f9ce6a1444836702a686c25eb
Darius K <darius.kaya@gmail.com>
2024-02-15 20:09:59 +0100

    bla bla

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const url = require("url");
 const fs = require("fs");
 const guitars = require("./guitars.js")
 const guitarists = require("./guitarists.js")
-console.log(guitarists);
 
 let server = http.createServer((req, res) => {
     let parsedUrl = url.parse(req.url, true);
@@ -11,15 +10,19 @@ let server = http.createServer((req, res) => {
     if (req.url === "/") {        
         res.writeHead(200, "The response is OK", { "Content-Type": "html" });
         res.write("<h1>Welcome to best guitar page of the entire internet</h1>");
-        res.write("<p>Add '/guitars' in the URL to see a list of models, and add '/guitars/model-of-your-choice' to see more info on a specific model. You can also add '/guitarists' to show a bunch of legendary guitarists, or '/guitarists?guitarist=guitarist-name' to show a specific guitarist.</p>")
+        res.write("<p>Add '/guitars' in the URL to see a list of models, and add '/guitars/model-of-your-choice' to see more info on a specific model. You can also add '/guitarists' to show a bunch of legendary guitarists, or '/guitarists?guitarist=guitarist-name' to search for a specific guitarist.</p>")
         res.end();
 
     } else if (req.url === "/guitars") {
-        fs.readFile("./guitars.html", (err, data) => {
-            if (!data) res.write(`${err}. No guitars were found`)
+        fs.readFile("./guitarss.html", (err, data) => {
+            if (err) {
+                res.writeHead(500, "File not found", { "Content-Type": "text/plain" });
+                res.write(`${err}. No guitars were found`);
+                res.end();
+            }
             res.writeHead(200, "The response is OK", { "Content-Type": "html" });
             res.end(data);
-        })  
+        });  
 
     } else if (parsedUrl.pathname.startsWith("/guitars/")) {
         const modelName = parsedUrl.pathname.split("/")[2];
@@ -39,12 +42,20 @@ let server = http.createServer((req, res) => {
     } else if (parsedUrl.pathname.startsWith("/guitarists")) {        
         if (parsedUrl.query.guitarist) {
             const guitaristName = parsedUrl.query.guitarist;
-            const filteredGuitarists = guitarists.filter(guitarist => {
+            const filteredGuitarist = guitarists.filter(guitarist => {
                 return guitarist.path.includes(guitaristName);
             });
-            res.writeHead(200, "The response is OK", { "Content-Type": "application/json" });
-            res.write(JSON.stringify(filteredGuitarists));
-            res.end();
+            
+            if (filteredGuitarist.length > 0) {
+                res.writeHead(200, "The response is OK", { "Content-Type": "application/json" });
+                res.write(JSON.stringify(filteredGuitarist));
+                res.end();
+            } else {
+                res.writeHead(404, "Not found");
+                res.write("Guitarist not found. Try different guitarist.");
+                res.end();
+            }
+
         } else {
             res.writeHead(200, "The response is OK", { "Content-Type": "application/json" });
             res.write(JSON.stringify(guitarists));
